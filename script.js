7165acc232feca28072a9d8e43abbb28ff4e0475
hasan <mdhasanhk.98@gmail.com>
2023-05-25 00:30:08 +0600

    array and sorting

--- script.js
+++ script.js
@@ -41,11 +41,74 @@ const hk = function () {
 // const a = "hasan";
 // const b = "nader";
 
+//////////////////////////////////////
+//Number
 const x = 500;
 const y = new Number(500);
 
-console.log(y);
+// console.log(y);
 
-const m = 120;
+let m = new Number(120);
 const newM = m.toString();
-console.log(m, newM);
+const fixed = m.toFixed();
+const parseInT = parseInt(newM);
+
+// console.log(typeof newM);
+// console.log(typeof parseInT);
+//////////////////////////////////////
+
+//////////////////////////////////////
+//array
+// const cars = new Array(["Saab", "Volvo", "BMW", "Audi"]);
+// cars[4] = "Audi";
+// console.log(cars.indexOf("BMW"));
+// console.log(cars[cars.length - 1]);
+
+// const myFunc = function (value) {
+//   console.log(value);
+// };
+// cars.forEach(function (value) {
+//   console.log(value);
+// });
+
+// for (let i = 0; i < cars.length; i++) {
+//   const element = cars[i];
+//   console.log(element);
+// }
+
+// const fruits = ["apple", "mango", "banana", "lemon"];
+// console.log(fruits.toString());
+// console.log(fruits.join(" * "));
+
+// const a = fruits.pop();
+// console.log(a);
+// const j = fruits.splice(2, 0, "lichi", "tree");
+// console.log(fruits, j);
+// const cut = fruits.splice(0, 1);
+// console.log(cut);
+
+// const myGirls = ["Cecile", "Lone"];
+// const myBoys = ["Emil", "Tobias", "Linus"];
+// const myFruits = ["apple", "mango", "banana", "lemon"];
+
+// const myChildren = myGirls.concat(myBoys, myFruits);
+// console.log(myChildren);
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+const sortFruits = fruits.sort();
+console.log(sortFruits);
+
+const sortReverse = fruits.reverse();
+console.log(sortReverse);
+
+const cars = [
+  { type: "Volvo", year: 2016 },
+  { type: "Saab", year: 2001 },
+  { type: "BMW", year: 2010 },
+];
+
+const hm = cars.sort(function (a, b) {
+  return a.year - b.year;
+});
+console.log(hm);
